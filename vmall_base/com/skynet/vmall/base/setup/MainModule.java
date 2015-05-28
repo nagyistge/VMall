81e08811af3beb36b65aee2d40d84974298e2a61
skynetbird <skynetbird@126.com>
2015-05-28 23:56:09 +0800

    更新

---
+++ vmall_base/com/skynet/vmall/base/setup/MainModule.java
@@ -0,0 +1,14 @@
+package com.skynet.vmall.base.setup;
+
+import org.nutz.mvc.annotation.IocBy;
+import org.nutz.mvc.annotation.Modules;
+import org.nutz.mvc.ioc.provider.ComboIocProvider;
+
+@Modules(packages =
+{ "com.skynet.vmall", "com.skynet.app", "com.vmall" }, scanPackage = true)
+@IocBy(type = ComboIocProvider.class, args =
+{ "*org.nutz.ioc.loader.json.JsonLoader", "ioc.js", "*org.nutz.ioc.loader.annotation.AnnotationIocLoader", "com.skynet", "com.skynet.vmall" })
+public class MainModule
+{
+
+}
