62ebf2afe894c112aea1a2896abf062e49f8d536
skynetbird <skynetbird@126.com>
2015-05-28 23:13:27 +0800

    扩展用户属性

--- skynet_organ/com/skynet/app/organ/pojo/User.java
+++ skynet_organ/com/skynet/app/organ/pojo/User.java
@@ -6,105 +6,28 @@ import org.nutz.dao.entity.annotation.Table;
 import com.skynet.framework.pojo.IdEntity;
 
 @Table("T_SYS_USER")
-public class User extends IdEntity {
-	
+public class User extends IdEntity
+{
+
 	@Column
 	private String loginname; // 登录名
 
 	@Column
 	private String cname; // 姓名
-	
-	@Column	
+
+	@Column
 	private String password; // 密码
-	
-	@Column	
+
+	@Column
 	private String isusing; // 可用标志
-	
-	@Column	
+
+	@Column
 	private String ordernum; // 排序
 	
-	@Column	
-	private String ownerdept; // 所属部门，临时字段，后期删除
+	@Column
+	private String weixinno; // 微信号
 	
 	@Column
-	private String ownerorg; // 所属机构，临时字段，后期删除
-	
-	@Column	
-	private String deptname; // 所属部门，临时字段，后期删除
-	
-	@Column
-	private String orgname; // 所属机构，临时字段，后期删除	
-
-	public String getLoginname() {
-		return loginname;
-	}
-
-	public void setLoginname(String loginname) {
-		this.loginname = loginname;
-	}
-
-	public String getCname() {
-		return cname;
-	}
-
-	public void setCname(String cname) {
-		this.cname = cname;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getIsusing() {
-		return isusing;
-	}
-
-	public void setIsusing(String isusing) {
-		this.isusing = isusing;
-	}
-
-	public String getOrdernum() {
-		return ordernum;
-	}
-
-	public void setOrdernum(String ordernum) {
-		this.ordernum = ordernum;
-	}
-
-	public String getOwnerdept() {
-		return ownerdept;
-	}
-
-	public void setOwnerdept(String ownerdept) {
-		this.ownerdept = ownerdept;
-	}
-
-	public String getOwnerorg() {
-		return ownerorg;
-	}
-
-	public void setOwnerorg(String ownerorg) {
-		this.ownerorg = ownerorg;
-	}
-
-	public String getDeptname() {
-		return deptname;
-	}
-
-	public void setDeptname(String deptname) {
-		this.deptname = deptname;
-	}
-
-	public String getOrgname() {
-		return orgname;
-	}
-
-	public void setOrgname(String orgname) {
-		this.orgname = orgname;
-	}
+	private String mobile; // 手机号
 
 }
