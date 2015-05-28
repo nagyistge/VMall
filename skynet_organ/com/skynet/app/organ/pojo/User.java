9991a749e2c11621c1413a3f651885e80f18da5c
skynetbird <skynetbird@126.com>
2015-05-28 23:14:02 +0800

    更新

--- skynet_organ/com/skynet/app/organ/pojo/User.java
+++ skynet_organ/com/skynet/app/organ/pojo/User.java
@@ -30,4 +30,73 @@ public class User extends IdEntity
 	@Column
 	private String mobile; // 手机号
 
+	public String getLoginname()
+	{
+		return loginname;
+	}
+
+	public void setLoginname(String loginname)
+	{
+		this.loginname = loginname;
+	}
+
+	public String getCname()
+	{
+		return cname;
+	}
+
+	public void setCname(String cname)
+	{
+		this.cname = cname;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+
+	public String getIsusing()
+	{
+		return isusing;
+	}
+
+	public void setIsusing(String isusing)
+	{
+		this.isusing = isusing;
+	}
+
+	public String getOrdernum()
+	{
+		return ordernum;
+	}
+
+	public void setOrdernum(String ordernum)
+	{
+		this.ordernum = ordernum;
+	}
+
+	public String getWeixinno()
+	{
+		return weixinno;
+	}
+
+	public void setWeixinno(String weixinno)
+	{
+		this.weixinno = weixinno;
+	}
+
+	public String getMobile()
+	{
+		return mobile;
+	}
+
+	public void setMobile(String mobile)
+	{
+		this.mobile = mobile;
+	}
 }
