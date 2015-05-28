cdc6bae6560fd6db12a9e55b71ad23d49cb380d0
skynetbird <skynetbird@126.com>
2015-05-28 23:27:40 +0800

    初始化

---
+++ vmall_system/com/skynet/vmall/system/system/action/SystemAction.java
@@ -0,0 +1,31 @@
+package com.skynet.vmall.system.system.action;
+
+import java.util.Map;
+
+import org.nutz.dao.util.Daos;
+import org.nutz.ioc.loader.annotation.Inject;
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.Ok;
+
+import com.skynet.app.organ.service.OrganService;
+import com.skynet.framework.action.BaseAction;
+
+@IocBean
+@At("/system/system")
+public class SystemAction extends BaseAction
+{
+	@Inject
+	private OrganService organService;
+	
+	@At("/inittable")
+	@Ok("json")
+	public Map inittable() throws Exception
+	{
+		// 初始化表结构
+		Daos.createTablesInPackage(organService.dao(), "com.skynet.app.organ.pojo", true);
+		Daos.createTablesInPackage(organService.dao(), "com.skynet.vmall.base.pojo", true);
+		Daos.createTablesInPackage(organService.dao(), "com.skynet.app.dictionary.pojo", true);
+		return ro;
+	}
+}
