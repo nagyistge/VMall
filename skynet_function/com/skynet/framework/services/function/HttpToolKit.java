18174ecbc47ba8194eccd2e9f06d00e557254d07
Administrator <Administrator@skynethp>
2015-06-17 06:15:49 +0800

    更新

---
+++ skynet_function/com/skynet/framework/services/function/HttpToolKit.java
@@ -0,0 +1,32 @@
+package com.skynet.framework.services.function;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class HttpToolKit
+{
+	public static String getIpAddr(HttpServletRequest request)
+	{
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
+		{
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
+		{
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
+		{
+			ip = request.getHeader("HTTP_CLIENT_IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
+		{
+			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
+		{
+			ip = request.getRemoteAddr();
+		}
+		return ip;
+	}
+}
