69941d22444796016e4cf0a5d1b259d2c5c24e58
skynetbird <skynetbird@126.com>
2015-07-05 20:23:24 +0800

    更新商品发布

---
+++ vmall_base/com/skynet/vmall/base/pojo/GoodsClassSpecValue.java
@@ -0,0 +1,62 @@
+package com.skynet.vmall.base.pojo;
+
+import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.Table;
+
+import com.skynet.framework.pojo.IdEntity;
+
+@Table("T_APP_GOODSCLASSSPECVAL")
+public class GoodsClassSpecValue extends IdEntity
+{
+	@Column
+	private String goodsclassid; // 商品分类标识
+	
+	@Column
+	private String goodsclassspecid; // 商品分类规格标识	
+	
+	@Column
+	private int sno; // 序号
+	
+	@Column
+	private String cvalue; // 规格内容
+
+	public String getGoodsclassid()
+	{
+		return goodsclassid;
+	}
+
+	public void setGoodsclassid(String goodsclassid)
+	{
+		this.goodsclassid = goodsclassid;
+	}
+
+	public String getGoodsclassspecid()
+	{
+		return goodsclassspecid;
+	}
+
+	public void setGoodsclassspecid(String goodsclassspecid)
+	{
+		this.goodsclassspecid = goodsclassspecid;
+	}
+
+	public int getSno()
+	{
+		return sno;
+	}
+
+	public void setSno(int sno)
+	{
+		this.sno = sno;
+	}
+
+	public String getCvalue()
+	{
+		return cvalue;
+	}
+
+	public void setCvalue(String cvalue)
+	{
+		this.cvalue = cvalue;
+	}	
+}
